f3d93c394735d3a5204651d00e62b84cfd59395b
iliampav <iliamfer@hotmail.com>
2023-09-02 14:04:35 -0300

    change to https

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,6 @@ import "swiper/css";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-// export function reportWebVitals(metric: NextWebVitalsMetric) {
-//   if (metric.label === 'web-vital') {
-    
-//   }
-// }
-
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
