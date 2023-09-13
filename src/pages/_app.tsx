fb3d6e485f040072d0932f95142dd5e5740a3348
Iliam Ferreira Pavkovic <65317849+iliampav@users.noreply.github.com>
2023-09-12 21:41:28 -0300

    changes to add gtm on pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,16 @@ import '../styles/fonts.css'
 import "swiper/css";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import { useEffect } from 'react';
+import { analytics } from '../components/gtmFile/analytics';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    console.log('aqui')
+    analytics(window, document, 'script', 'dataLayer', 'GTM-MZG2JHG');
+  },[])
+
   return <Component {...pageProps} />
 }
 
