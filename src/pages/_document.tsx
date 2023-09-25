df5a52d008765d1e880d23363e2b441eacebe696
Iliam Ferreira Pavkovic <65317849+iliampav@users.noreply.github.com>
2023-09-25 20:47:52 -0300

    test if it calls the scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" /> 
                     <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
-                    <Script id="gtm-script" strategy="afterInteractive">
+                    {/* <Script id="gtm-script" strategy="afterInteractive">
                         {
                             `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -19,7 +19,7 @@ export default class MyDocument extends Document {
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                             })(window,document,'script','dataLayer','GTM-MZG2JHG');`
                         }
-                    </Script>
+                    </Script> */}
                 </Head>
                 <body>
                     <Main />
