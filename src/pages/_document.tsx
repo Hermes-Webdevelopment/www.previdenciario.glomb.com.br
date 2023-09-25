0c218abac93ca1265471299b83c72483b9315ddd
Iliam Ferreira Pavkovic <65317849+iliampav@users.noreply.github.com>
2023-09-25 20:55:24 -0300

    change script to be outside the body

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,15 +11,6 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" /> 
                     <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
-                    {/* <Script id="gtm-script" strategy="afterInteractive">
-                        {
-                            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-MZG2JHG');`
-                        }
-                    </Script> */}
                 </Head>
                 <body>
                     <Main />
@@ -28,6 +19,15 @@ export default class MyDocument extends Document {
                         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZG2JHG" height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe>
                     </noscript>
                 </body>
+                    <Script id="gtm-script" strategy="afterInteractive">
+                        {
+                            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-MZG2JHG');`
+                        }
+                    </Script>
             </Html>
         ) 
     } 
