7219ca5bd09d04373992449acce79ef4cb331e9f
Iliam Ferreira Pavkovic <65317849+iliampav@users.noreply.github.com>
2023-09-26 20:01:09 -0300

    add a js caller for gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import { useEffect } from 'react'
+import { gtmCaller } from '../components/gtmCaller/gtmScript'
 
 
 export default class MyDocument extends Document {
     render() {
         
+        useEffect(() => {
+            gtmCaller()
+        },[])
+
         return ( 
             <Html lang="pt">
                 <Head>
@@ -12,15 +17,6 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" /> 
                     <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
-                    <Script id="gtm-script" strategy="afterInteractive" defer>
-                        {
-                            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=false;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-MZG2JHG');`
-                        }
-                    </Script>
                 </Head>
                 <body>
                     <Main />
