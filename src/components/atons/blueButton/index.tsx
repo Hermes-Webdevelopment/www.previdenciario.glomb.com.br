9d78d31c8b8d972403c85251618c88d723c8f375
Iliam Ferreira Pavkovic <65317849+iliampav@users.noreply.github.com>
2023-10-05 21:08:49 -0300

    change on buton to be under the footer

--- src/components/atons/blueButton/index.tsx
+++ src/components/atons/blueButton/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import styles from './styles.module.scss';
-import { ReactNode, useEffect, useState } from 'react';
+import { ReactNode } from 'react';
 
 interface buttonProps {
     buttonLink: string; 
@@ -14,37 +14,9 @@ export default function BlueButton(props: buttonProps) {
 
     const {buttonLink, buttonText, isFloating = false, cleanMode = false, transparentMode = false} = props
 
-    const [isVisible, setIsVisible] = useState(false)
-
-    let height = 0
-    let windowHeight = 0
-    
-    useEffect(() => {
-        let elem = document.getElementById("footerContainer")
-        height = elem ? elem.getBoundingClientRect().top : 0
-        windowHeight = window.innerHeight
-
-        window.addEventListener("scroll", listenToScroll);
-        return () =>
-           window.removeEventListener("scroll", listenToScroll);
-    }, [])
-
-    const listenToScroll = () => {
-
-        let heightToHideFrom = height - windowHeight * 2;
-
-        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-      
-        if (winScroll > heightToHideFrom) {
-             setIsVisible(true);
-        } else {
-             setIsVisible(false);
-        }
-    };
-
     return (
         <div className={`${styles.mainMode} ${styles.forceChange} ${isFloating ? styles.floatingButton : ''} ${cleanMode ? styles.btnClean : ''} ${transparentMode ? styles.btnTransparent : ''}`}>
-            <Link href={buttonLink} className={`${styles.buttonContainer} ${isVisible && isFloating ? styles.changeOpacity : ''}`} prefetch={false}>
+            <Link href={buttonLink} className={`${styles.buttonContainer}`} prefetch={false}>
                 <p>{buttonText}</p>
             </Link>
         </div>
