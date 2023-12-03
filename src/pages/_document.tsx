0a534dd8995f813623b640466e8099feb77f3a01
Iliam Ferreira Pavkovic <65317849+iliampav@users.noreply.github.com>
2023-12-03 12:08:44 -0300

    update robots and no index

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" /> 
                     <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+                    <meta name="robots" content="noindex"></meta>
                 </Head>
                 <body>
                     <Main />
